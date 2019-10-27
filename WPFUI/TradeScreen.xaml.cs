39d7ebfd922ce5e8839d57fa16891dbf1494a35a
Blake <rbpassmor@gmail.com>
2019-10-27 02:27:51 -0600

    add trade screen

---
+++ WPFUI/TradeScreen.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+using Engine.ViewModels;
+using System.Windows;
+
+
+namespace WPFUI
+{
+    public partial class TradeScreen : Window
+    {
+        public GameSession Session => DataContext as GameSession;
+        public TradeScreen()
+        {
+            InitializeComponent();
+        }
+        private void OnClick_Sell(object sender, RoutedEventArgs e)
+        {
+            GameItem item = ((FrameworkElement)sender).DataContext as GameItem;
+            if (item != null)
+            {
+                Session.CurrentPlayer.Gold += item.Price;
+                Session.CurrentTrader.AddItemToInventory(item);
+                Session.CurrentPlayer.RemoveItemFromInventory(item);
+            }
+        }
+        private void OnClick_Buy(object sender, RoutedEventArgs e)
+        {
+            GameItem item = ((FrameworkElement)sender).DataContext as GameItem;
+            if (item != null)
+            {
+                if (Session.CurrentPlayer.Gold >= item.Price)
+                {
+                    Session.CurrentPlayer.Gold -= item.Price;
+                    Session.CurrentTrader.RemoveItemFromInventory(item);
+                    Session.CurrentPlayer.AddItemToInvetory(item);
+                }
+                else
+                {
+                    MessageBox.Show("You don't have enough gold");
+                }
+            }
+        }
+        private void OnClick_Close(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
